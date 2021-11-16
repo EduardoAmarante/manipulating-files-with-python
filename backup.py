80a19c6f27b720371eb524d5e43d19b4b2b2c05a
Eduardo Amarante <eamarante80@gmail.com>
2021-11-16 02:23:20 -0300

    adicionada funcao 'separarPorTipo'

--- backup.py
+++ backup.py
@@ -2,7 +2,17 @@ from pathlib import Path
 import os,shutil, ctypes
 
 listaformatos = []
+formatosPlanilhas = ['.xlsx','.ods','.csv']
+formatosDocs = ['.odt','.docx','.pptx']
+formatoImages = ['.jpg','.jpeg','.JPG','.JPEG','.png']
 path = Path("C:/Users/Eduardo/Desktop/")
+
+def separarPorTipo(nomePasta,filename):
+                try:
+                    verificaPasta("G:/Meu Drive/{}".format(nomePasta)) 
+                    shutil.move(filename, "G:/Meu Drive/{}/{}".format(nomePasta, filename.name))
+                except WindowsError:
+                    ctypes.windll.user32.MessageBoxW(0, "O arquivo esta sendo utilizado por outro processo", "Erro no Script", 0)
 
 def verificaPasta(caminho):
     try:
@@ -14,13 +24,14 @@ def mover(listaformatos):
     for filename in path.glob('*'):
         
         if filename.suffix in listaformatos:
-            try:
-
-                verificaPasta("G:/Meu Drive/{}".format(filename.suffix)) 
-                shutil.move(filename, "G:/Meu Drive/{}/{}".format(filename.suffix,filename.name))
-            except WindowsError:
-                ctypes.windll.user32.MessageBoxW(0, "O arquivo esta sendo utilizado por outro processo", "Erro no Script", 0)
-
+            if filename.suffix in formatosPlanilhas:
+                separarPorTipo('Planilhas',filename)
+            elif filename.suffix in formatosDocs:
+                separarPorTipo('Documentos',filename)
+            elif filename.suffix in formatoImages:
+                separarPorTipo('Imagens',filename)
+            else:
+                separarPorTipo(filename.suffix,filename)
         else:
             print("não esta na lista")
 
