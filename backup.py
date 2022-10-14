eee95d0bcb0b5d57bec6171d11ebd0499d5735f7
Eduardo Amarante <eamarante80@gmail.com>
2022-10-14 19:21:49 -0300

    comments translated into english

--- backup.py
+++ backup.py
@@ -2,16 +2,16 @@ from pathlib import Path
 import shutil
 
 def move(nameFolder,filename):
-    # move os arquivos da area de trabalho para o caminho especificado
+    # moves files from the desktop to the path specified by the nameFolder variable
     shutil.move(filename, "C:/Users/{}/{}/{}".format(user,nameFolder, filename.name))
     print("Moving the file... {}".format(filename.name)) 
 
 def analyze_and_move():
-    # loop que verifica todos arquivos do path
+    # loop that checks all files in path(desktop)
     for filename in path.glob('*'):
-        #verifica se o formato de arquivo esta presente em uma das listas
-        # quando estiver presente ele envia o arquivo para o diretorio nomeado no
-        # primeiro argumento do move()
+        # checks if the file format is present in one of the lists
+        # when it is present it sends the file to the directory named in the
+        # first argument of move()
         if filename.suffix in formatoImages:
             move('Pictures',filename)
         elif filename.suffix in formatosDocs:
@@ -21,17 +21,17 @@ def analyze_and_move():
         elif filename.suffix in formatoDownloads:
             move('Downloads',filename)
            
-# defina aqui seu usuario windows
+# define your windows user here
 user = 'eamar'
 
-# listas organizadas de acordo com os tipos de arquivos a serem manipulados
+# lists organized according to the types of files to be handled
 formatosDocs = ['.odt','.docx','.pptx','.xlsx','.ods','.csv','.pdf']
 formatoImages = ['.jpg','.jpeg','.JPG','.JPEG','.png','.xcf','.HEIC']
 formatoVideo = ['.MP4','.mp4','.mkv','.mlt','.MOV','.HVEC']
 formatoDownloads = ['.zip','.exe']
 
-# gera o Path para ser compativel com o shutil
+# generates the Path to be compatible with shutil
 path = Path("C:/Users/{}/Desktop/".format(user))
 
-# funcao principal que roda o script
+# runs the script
 analyze_and_move()
